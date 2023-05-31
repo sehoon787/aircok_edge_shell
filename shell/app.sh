f86c29c143cf537c1cbbd4f7be480fce9e3cdebc
Gundue <66405643+Gundue@users.noreply.github.com>
2023-05-31 15:21:34 +0900

    Update app.sh

--- shell/app.sh
+++ shell/app.sh
@@ -2,11 +2,17 @@
 
 set -e
 
+# Read the versions from the JSON file
+version_file=~/version.json
+db_version=$(jq -r '.db_version' "$version_file")
+server_version=$(jq -r '.server_version' "$version_file")
+app_version=$(jq -r '.app_version' "$version_file")
+shell_version=$(jq -r '.shell_version' "$version_file")
+
 # Run ifconfig command and filter eth0 line
 mac_address=$(ifconfig eth0 | awk '/ether/ {gsub(/:/,"",$2); print $2}')
 
 # Read the app version from the JSON file
-app_version=$(jq -r '.version' ~/bundle/data/flutter_assets/version.json)
 target_image="aircok/aircok_edge_app:${app_version}"
 
 # Check if the Docker image exists
@@ -20,8 +26,9 @@ if ! sudo docker images | awk '{print $1}' | grep -q "$target_image"; then
     # Run Docker image
     sudo docker run -itd "$target_image"
     # Copy the app bundle from the Docker container
-    container_id=$(sudo docker ps -aqf "$target_image")
-    sudo docker cp "${container_id}:/app/AircokEdge/build/linux/x64/release/bundle" ~/
+    container_id=$(sudo docker ps -qf "ancestor=$target_image")
+	sudo docker cp "${container_id}":/app/AircokEdge/build/linux/arm64/release/bundle ~/
+	
     # Stop the Docker container
     sudo docker stop "${container_id}"
 
@@ -34,6 +41,6 @@ if ! sudo docker images | awk '{print $1}' | grep -q "$target_image"; then
 fi
 
 # Change directory to the app bundle
-cd ~/AircokEdge/aircok_edge_app/bundle
+cd ~/bundle
 # Run the app
 ./aircok_edge &
