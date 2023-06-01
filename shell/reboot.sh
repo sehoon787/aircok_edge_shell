9182d9286610d1232164fdc079c72e3121d41cfe
Alien Coder <59464137+sehoon787@users.noreply.github.com>
2023-06-01 16:06:35 +0900

    Update reboot.sh

--- shell/reboot.sh
+++ shell/reboot.sh
@@ -20,28 +20,26 @@ DOCKER_IMAGES=(
 
 # Iterate over each Docker image name
 for image_name in "${DOCKER_IMAGES[@]}"; do
-    # Check if the Docker image exists on Docker Hub
-    if sudo docker pull "${image_name}" &> /dev/null; then
-        echo "Docker image '${image_name}' exists on Docker Hub."
-        # Check if the Docker image already exists locally
-        if sudo docker images | awk '{print $1":"$2}' | grep -q "${image_name}"; then
-            echo "Docker image '${image_name}' is already present locally."
-        else
-            echo "Pulling the image..."
+    # Check if the Docker image already exists locally
+    if sudo docker images | awk '{print $1":"$2}' | grep -q "${image_name}"; then
+        echo "Docker image '${image_name}' is already present locally."
+        # Check if the Docker image exists on Docker Hub
+        if sudo docker pull "${image_name}" &> /dev/null; then
+            echo "Pulling '${image_name}' from Docker Hub."
+            sudo docker pull "${image_name}"
+            echo "✅ Downloaded '${image_name}' successfully."
+            state="true"
+
             # Remove previous version of the image if exists
             prev=$(sudo docker images --format "{{.Repository}}:{{.Tag}}" | grep "${image_name}")
             if [[ -n "$prev" ]]; then
                 sudo docker rmi -f "$prev"
                 echo "Previous version '$prev' has been forcefully removed."
             fi
-
-            sudo docker pull "${image_name}"
-            echo "✅ Downloaded '${image_name}' successfully."
-            state="true"
+        else
+            echo "Docker image '${image_name}' does not exist on Docker Hub."
+            echo "⛔ Image not pulled."
         fi
-    else
-        echo "Docker image '${image_name}' does not exist on Docker Hub."
-        echo "⛔ Image not pulled."
     fi
 done
 
