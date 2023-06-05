e91150132ac4b31ce1cf06265c473e5656bb6e00
sehoon787 <sehoon787@naver.com>
2023-06-05 06:19:01 +0000

    Update shell files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # aircok_edge_shell
-Last updated: 2023-06-05 06:03:43.199793534
+Last updated: 2023-06-05 06:19:01.533914735
--- shell/init.sh
+++ shell/init.sh
@@ -68,7 +68,6 @@ curl -o /home/aircok/version.json https://v3.aircok.com/web/edge/update?sn=$(ifc
 (crontab -l 2>/dev/null; echo "0 */6 * * * sudo curl -o /home/aircok/version.json https://v3.aircok.com/web/edge/update?sn=\$(ifconfig eth0 | awk '/ether/ {gsub(/:/,\"\",$2); print \$2}')") | crontab -
 (crontab -l 2>/dev/null; echo "0 2 * * * bash /home/aircok/aircok_edge_shell/shell/listener.sh") | crontab -
 
-
 # Register start shell
 sudo sh -c 'echo "#!/bin/bash\n\n/home/aircok/start.sh\n/home/aircok/aircok_edge_shell/shell/server.sh\n\nexit 0" > /etc/rc.local'
 
